890f2f6015aee2568d155e4db622e85f29d59b46
Weslley Campos <weslley.camphos@hotmail.com>
2021-06-05 18:00:08 -0300

    Visitor visit website

---
+++ spec/system/visitor_open_website_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Visitor visit homepage' do
+  it 'successfully' do
+
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'PayNow')
+    expect(page).to have_css('p', text: 'Simplifique o recebimento de cobranças '\
+                                          'e automatize seu negócio')
+    expect(page).to have_link('Cadastre-se')
+    expect(page).to have_link('Entrar')
+
+  end
+end
