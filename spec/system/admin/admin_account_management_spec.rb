21ef879418a6d7bef6805572830b9b58463c1e43
Weslley Campos <weslley.camphos@hotmail.com>
2021-06-06 00:03:43 -0300

    Administrator registers and logs into the platform

--- spec/system/admin/admin_account_management_spec.rb
+++ spec/system/admin/admin_account_management_spec.rb
@@ -3,28 +3,32 @@ require 'rails_helper'
 describe 'Admin account management' do
   context 'log in' do
     it 'successfuly' do
-      Admin.create!(email: 'admin@paynow.com', password: '123456')
+      Admin.create!(email: 'admin@paynow.com.br', password: '123456')
 
-      visit root_path
-      click_on 'Entrar'
-      fill_in 'Email', with: 'admin@paynow.com'
+      visit new_admin_session_path
+      within 'div#session' do
+        click_on 'Entrar'
+      end
+      fill_in 'Email', with: 'admin@paynow.com.br'
       fill_in 'Senha', with: '123456'
-      within 'form' do
+      within 'div#login' do
         click_on 'Entrar'
       end
 
       expect(page).to have_text('Login efetuado com sucesso')
-      expect(page).to have_text('Olá, admin@paynow.com')
+      expect(page).to have_text('Olá, admin@paynow.com.br')
       expect(current_path).to eq(root_path)
       expect(page).to_not have_link('Entrar')
       expect(page).to have_link('Sair')
     end
 
     it 'cannot have a blank fields' do
-      Admin.create!(email: 'admin@paynow.com', password: '123456')
+      Admin.create!(email: 'admin@paynow.com.br', password: '123456')
       
-      visit root_path
-      click_on 'Entrar'
+      visit new_admin_session_path
+      within 'div#session' do
+        click_on 'Entrar'
+      end
       fill_in 'Email', with: ''
       fill_in 'Senha', with: ''
       within 'form' do
@@ -36,14 +40,14 @@ describe 'Admin account management' do
 
   context 'logout' do
     it 'successfully' do
-      admin = Admin.create!(email: 'admin@paynow.com', password: '123456')
+      admin = Admin.create!(email: 'admin@paynow.com.br', password: '123456')
 
       login_as admin, scope: :admin
       visit root_path
       click_on 'Sair'
 
       expect(page).to have_text('Saiu com sucesso')
-      expect(page).to_not have_text('Olá, admin@paynow.com')
+      expect(page).to_not have_text('Olá, admin@paynow.com.br')
       expect(current_path).to eq(root_path)
       expect(page).to have_link('Entrar')
       expect(page).to_not have_link('Sair')
@@ -52,12 +56,11 @@ describe 'Admin account management' do
 
   context 'Admin forgot password' do
     it 'receive reset email successfully' do
-      Admin.create!(email: 'admin@user.com', password: '123456')
+      Admin.create!(email: 'admin@paynow.com.br', password: '123456')
 
-      visit root_path
-      click_on 'Entrar'
+      visit new_admin_session_path
       click_on 'Esqueceu sua senha?'
-      fill_in 'Email', with: 'admin@paynow.com'
+      fill_in 'Email', with: 'admin@paynow.com.br'
       click_on 'Envie-me instruções de redefinição de senha'
 
       expect(current_path).to eq(new_admin_session_path)
@@ -65,10 +68,10 @@ describe 'Admin account management' do
     end
 
     it 'and change password' do
-      admin = Admin.create!(email: 'user@user.com', password: '123456')
+      admin = Admin.create!(email: 'admin@paynow.com.br', password: '123456')
       token = admin.send_reset_password_instructions
 
-      visit edit_user_password_path(reset_password_token: token)
+      visit edit_admin_password_path(reset_password_token: token)
       fill_in 'Nova Senha', with: '12345678'
       fill_in 'Confirmar Nova Senha', with: '12345678'
       click_on 'Mudar Minha Senha'
--- spec/system/suport/login_macros.rb
+++
@@ -1,9 +0,0 @@
-module LoginMacros
-  def login_admin
-    admin = Admin.create!(email: 'admin@paynow.com',
-                          password: '123456')
-
-    login_as admin, scope: :admin
-
-  end
-end
