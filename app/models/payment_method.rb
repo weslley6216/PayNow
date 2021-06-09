e360c90cc20fdcb9b4f7f52545d86859908b15fd
Weslley Campos <weslley.camphos@hotmail.com>
2021-06-09 17:48:37 -0300

    Adding icon to payment methods automatically

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,7 +1,24 @@
 class PaymentMethod < ApplicationRecord
+  after_create_commit :attach_icon
 
-  has_one_attached :icone
+  has_one_attached :icon
 
-  validates :name, :tax, :max_tax, presence: true
-  validates :name, uniqueness: true
+  enum form_of_payment: { bank_slip: 1, credit_card: 2, pix: 3 }
+
+  private
+
+  def attach_icon
+    return if icon.attached?
+
+    if PaymentMethod.last.bank_slip?
+      icon.attach(io: File.open(Rails.root.join('app/assets/images/bank_slip.png')),
+                  filename: 'pix.png')
+    elsif PaymentMethod.last.credit_card?
+      icon.attach(io: File.open(Rails.root.join('app/assets/images/credit_card.jpg')),
+                  filename: 'pix.png')
+    elsif PaymentMethod.last.pix?
+      icon.attach(io: File.open(Rails.root.join('app/assets/images/pix.png')),
+                  filename: 'pix.png')
+    end
+  end
 end
